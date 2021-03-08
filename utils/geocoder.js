8093883a32e87ef0e1e8ba9d18997a26710809d8
manvendra-rajpoot <manvendra141986@gmail.com>
2021-03-08 21:22:17 +0530

    node-geocoder

---
+++ utils/geocoder.js
@@ -0,0 +1,13 @@
+const NodeGeocoder = require('node-geocoder');
+
+const options = {
+    provider: process.env.GEOCODER_PROVIDER,
+    httpAdapter: 'https',
+    // fetch: customFetchImplementation,
+    apiKey: process.env.GEOCODER_API_KEY, // for Mapquest
+    formatter: null // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder; 
