d9f5bf4128d90328959140e1e17819cd516c738f
manvendra-rajpoot <manvendra141986@gmail.com>
2021-03-15 14:35:23 +0530

    aggregate func., calculating avgCost

--- models/Course.js
+++ models/Course.js
@@ -40,5 +40,42 @@ const CourseSchema = new mongoose.Schema({
     },
 });
 
+//static method to get avg   pf courses tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId){
+    // console.log('Calculating Avg cost....'.blue);
+    
+    const obj = await this.aggregate([
+        {
+            $match: {
+                bootcamp: bootcampId,
+            }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' },
+            }
+        }
+    ]);
+    
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost)
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+//call getAverageCost after save
+CourseSchema.post('save', function(){
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+//call getAverageCost before remove
+CourseSchema.pre('remove', function(){
+    this.constructor.getAverageCost(this.bootcamp); 
+});
+
 
 module.exports = mongoose.model('Course', CourseSchema);    
